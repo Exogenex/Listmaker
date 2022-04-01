ac2d41168cc31308da257b3a0363de9c2fa9c7ec
Exogenex <mpenni6@live.spcollege.edu>
2022-03-31 23:46:26 -0400

    End of Ch. 7

---
+++ app/src/main/java/com/exogenex/listmaker/ui/main/ListSelectionViewHolder.kt
@@ -0,0 +1,7 @@
+package com.exogenex.listmaker.ui.main
+
+import androidx.recyclerview.widget.RecyclerView
+import com.exogenex.listmaker.databinding.ListSelectionViewHolderBinding
+
+class ListSelectionViewHolder(val binding: ListSelectionViewHolderBinding) : RecyclerView.ViewHolder(binding.root) {
+}
--- app/src/main/java/com/exogenex/listmaker/ui/main/MainFragment.kt
+++ app/src/main/java/com/exogenex/listmaker/ui/main/MainFragment.kt
@@ -6,23 +6,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.exogenex.listmaker.R
+import com.exogenex.listmaker.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
-
-    private lateinit var binding: MainFragmentBinding
 
     companion object {
         fun newInstance() = MainFragment()
     }
 
+    private lateinit var binding: MainFragmentBinding
     private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = MainFragmentBinding.inflate(inflater, container, false)
+
+        binding.listsRecyclerview.layoutManager = LinearLayoutManager(requireContext())
+        binding.listsRecyclerview.adapter = ListSelectionRecyclerViewAdapter()
+
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
